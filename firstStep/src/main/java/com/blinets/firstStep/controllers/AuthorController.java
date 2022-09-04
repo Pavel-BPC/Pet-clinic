3ce49453a94985c7873a946c4610046d42c8a210
PavelBlinets <pavelblinets@gmail.com>
2022-09-04 11:58:36 +0300

    Thymeleaf for author

---
+++ firstStep/src/main/java/com/blinets/firstStep/controllers/AuthorController.java
@@ -0,0 +1,24 @@
+package com.blinets.firstStep.controllers;
+
+import com.blinets.firstStep.repositories.AuthorRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class AuthorController {
+
+    private final AuthorRepository authorRepository;
+
+    public AuthorController(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    @RequestMapping("/authors")
+    public String getAuthor(Model model) {
+
+        model.addAttribute("authors", authorRepository.findAll());
+
+        return "authors/list";
+    }
+}
