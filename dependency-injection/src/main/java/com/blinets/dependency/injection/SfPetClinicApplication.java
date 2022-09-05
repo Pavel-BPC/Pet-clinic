9c0b1c39cb261f3c7565d9616fc1f3ce47817dae
PavelBlinets <pavelblinets@gmail.com>
2022-09-05 16:24:33 +0300

    Spring Bean Scope

--- dependency-injection/src/main/java/com/blinets/dependency/injection/SfPetClinicApplication.java
+++ dependency-injection/src/main/java/com/blinets/dependency/injection/SfPetClinicApplication.java
@@ -1,6 +1,8 @@
 package com.blinets.dependency.injection;
 
 import com.blinets.dependency.injection.controllers.*;
+import com.blinets.dependency.injection.services.scope.PrototypeBean;
+import com.blinets.dependency.injection.services.scope.SingletonBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
@@ -37,6 +39,16 @@ public class SfPetClinicApplication {
         PetController petController = ctx.getBean("petController", PetController.class);
         System.out.println(petController.whichPetIsTheBest());
 
+        System.out.println("---- Bean Scopes ---------");
+        SingletonBean singletonBean1 = ctx.getBean(SingletonBean.class);
+        System.out.println(singletonBean1.getMyScope());
+        SingletonBean singletonBean2 = ctx.getBean(SingletonBean.class);
+        System.out.println(singletonBean2.getMyScope());
+
+        PrototypeBean prototypeBean1 = ctx.getBean(PrototypeBean.class);
+        System.out.println(prototypeBean1.getMyScope());
+        PrototypeBean prototypeBean2 = ctx.getBean(PrototypeBean.class);
+        System.out.println(prototypeBean2.getMyScope());
 
     }
 
