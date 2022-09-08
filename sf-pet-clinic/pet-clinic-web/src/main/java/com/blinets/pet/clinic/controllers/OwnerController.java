71bf8f56723fda552cd060411c290e962ecc9d21
PavelBlinets <pavelblinets@gmail.com>
2022-09-08 10:01:02 +0300

    list of vet and owner index page

--- sf-pet-clinic/pet-clinic-web/src/main/java/com/blinets/pet/clinic/controllers/OwnerController.java
+++ sf-pet-clinic/pet-clinic-web/src/main/java/com/blinets/pet/clinic/controllers/OwnerController.java
@@ -1,15 +1,23 @@
 package com.blinets.pet.clinic.controllers;
 
+import com.blinets.pet.clinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/owners")
 public class OwnerController {
 
-    @RequestMapping({"", "/", "/index", "/index.html"})
-    public String listPetOwners() {
+    private final OwnerService  ownerService;
 
+    public OwnerController(OwnerService ownerService) {
+        this.ownerService = ownerService;
+    }
+
+    @RequestMapping({"", "/", "/index", "/index.html"})
+    public String listPetOwners(Model model) {
+        model.addAttribute("owners", ownerService.findAll());
         return "owners/index";
     }
 }
