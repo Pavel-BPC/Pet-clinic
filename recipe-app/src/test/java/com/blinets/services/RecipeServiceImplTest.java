7370bdcff58e3dfd992aa1bd681b9d436f8c285d
PavelBlinets <pavelblinets@gmail.com>
2022-09-09 14:04:14 +0300

    mockito example

---
+++ recipe-app/src/test/java/com/blinets/services/RecipeServiceImplTest.java
@@ -0,0 +1,38 @@
+package com.blinets.services;
+
+import com.blinets.domain.Recipe;
+import com.blinets.repositories.RecipeRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+class RecipeServiceImplTest {
+
+    RecipeServiceImpl recipeService;
+    @Mock
+    RecipeRepository recipeRepository;
+
+    @BeforeEach
+    void setUp() {
+        MockitoAnnotations.openMocks(this);
+        recipeService = new RecipeServiceImpl(recipeRepository);
+    }
+
+    @Test
+    void getRecipes() {
+        Set<Recipe> recipesData = Set.of(new Recipe());
+        when(recipeRepository.findAll()).thenReturn(recipesData);
+
+        Set<Recipe> recipes = recipeService.getRecipes();
+
+        assertEquals(1, recipes.size());
+        verify(recipeRepository, times(1)).findAll();
+
+    }
+}
